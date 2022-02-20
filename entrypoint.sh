48544631be8db0f66126c3222ff9d54b59dafbd5
Robert Kempton <r@snow87.com>
2022-02-20 15:22:55 -0700

    fix after ready hook add logs in /tmp for all hook actions

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 if [ -f "/hooks/before_start.sh" ]; then
-  bash /hooks/before_start.sh
+  bash /hooks/before_start.sh > /tmp/hooks_before_start.sh.log 2>&1
 fi
 
-nohup bash /runAfterReady.sh & disown
+nohup bash /runAfterReady.sh > /tmp/hooks_runAfterReady.sh.log 2>&1 & disown
 
 exec /usr/local/bin/kind-entrypoint "$@"
--- runAfterReady.sh
+++ runAfterReady.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
-schedulerPod="$(kubectl get pods -n kube-system|grep kube-scheduler|awk '{print $1}')"
+function getSchedulerPod(){
+  kubectl get pods -n kube-system|grep kube-scheduler|awk '{print $1}'
+}
 
-while [[ "$(kubectl get pod -n kube-system $schedulerPod -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}')" != "True" ]]; do
+function isReady(){
+  kubectl get pod -n kube-system $1 -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}'
+}
+
+while [[ "$(isReady $(getSchedulerPod))" != "True" ]]; do
  sleep 1
 done
 
 if [ -f "/hooks/after_ready.sh" ]; then
-  bash /hooks/after_ready.sh
+  bash /hooks/after_ready.sh > /tmp/hooks_after_ready.sh.log 2>&1
 fi
